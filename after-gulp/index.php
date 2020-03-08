8ab100fcb6b1dc4121387a1244c519c8c076257f
danwalt2000 <danwalt@mail.ru>
2020-03-08 23:05:38 +0300

    Initial commit

---
+++ after-gulp/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="shortcut icon" href="./style/images/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="./style/fonts.min.css">
+  <link rel="stylesheet" href="./style/style.min.css">
+</head>
+<body>
+    <?php include "./header.html"; ?>
+
+    <?php include "./choose-beacons.html"; ?>  
+    
+    <?php include './steps.html'?>
+  
+    <?php include './footer.html'?>
+
+    <script type="text/javascript" src="./js/jquery.js"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
+</body>
+</html>
